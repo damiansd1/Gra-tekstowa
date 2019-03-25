6f54898b0b081ee16eeaf73763ee1b63679132e5
damiansd1 <43549705+damiansd1@users.noreply.github.com>
2019-03-25 19:36:36 +0100

    Add files via upload

--- Gra/main.cpp
+++ Gra/main.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 float HP2,ATK2,mana;
 char komenda;
-int zdarzenie;//0-walka 1-nic 2-przedmiot
+char zdarzenie;//w-walka n-nic p-przedmiot
 int d = 20;
 int n = 10;
 class Wrog
@@ -18,8 +18,7 @@ class Wrog
     string typ;
     Wrog()
     {
-    HP = rand()%d +n;
-    ATK = HP2*0.1;
+
     rodzaj = rand()%5 +1;
     if (rodzaj==1)
     {
@@ -41,24 +40,46 @@ class Wrog
     {
         typ="szczur";
     }
+    int sila = 10;
+    int agil = 10;
+    int intl = 11;
+
+    HP =10 + sila + rand()%d +n;
+    ATK = 2 + sila*0.1 + rand()%d*0.1 +n*0.1;
+
+
     }
 
 };
+int fail(int WrogATK,int agil2int,int Wrogagil)
+    {
+        int fail1;
+        for (1,WrogATK)
+            if (rand()%100<(agil2int-Wrogagil))
+                fail1 = fail1+1;
+            if (rand()%100<(Wrogagil-agil2int))
+                fail1 = fail1-1;
+        return fail1;
+    };
 int main()
 {
     srand (time(NULL));
-    HP2 = rand()%d +n;
-   while(HP2>0)
-    {
-    zdarzenie=(rand()+rand()/54)%2;
-    if(zdarzenie==0)//walka
-    {
+
+    int sila2 = 10;
+    int agil2 = 60;
+    int intl2 = 10;
+
+    HP2 = sila2 + 10 + rand()%d +n;
+    ATK2 = 2 + sila2*0.1 + rand()%d*0.1
+     +n*0.1;
     Wrog stwor;
-    ATK2 = stwor.HP*0.1;
+
+
     cout << stwor.HP <<","<<HP2 << endl;
     cout << stwor.ATK<<","<<ATK2 << endl;
     mana=3;
     cout <<" Spotkales "<<stwor.typ<<" ma "<<stwor.HP<<" zycia i "<<stwor.ATK<<" ataku "<<endl;
+     cout <<" Ty masz "<<HP2<<" zycia "<<mana<<" many i "<<ATK2<<" ataku "<<endl
 do
     {
         cout<< "Ilosc many: "<<mana<<endl;
@@ -69,7 +90,7 @@ do
         {if (mana>0)
         {
         stwor.HP = stwor.HP-ATK2*2;
-    HP2 = HP2-stwor.ATK;
+    HP2 = HP2-(stwor.ATK-fail(Wrog.ATK,agil2,Wrog.agil));
             mana = mana - 1;
                 if(HP2<=0) HP2=0;
         else if(stwor.HP<0) stwor.HP=0;
@@ -81,7 +102,7 @@ do
         else if (mana<=0)
         {
              cout<<"Brak many"<< endl;
-            HP2 = HP2-stwor.ATK;
+            HP2 = HP2-(stwor.ATK-fail(Wrog.ATK,agil2,Wrog.agil));
             if(HP2<=0) HP2=0;
         else if(stwor.HP<0) stwor.HP=0;
         else if ((HP2>0)&&(stwor.HP>0))cout << stwor.HP <<","<<HP2 << endl;
@@ -95,7 +116,7 @@ do
         case 'a':
         {
         stwor.HP = stwor.HP-ATK2;
-        HP2 = HP2-stwor.ATK;
+        HP2 = HP2-(stwor.ATK-fail(Wrog.ATK,agil2,Wrog.agil));
         if(HP2<=0) HP2=0;
         else if(stwor.HP<0) stwor.HP=0;
         else if ((HP2>0)&&(stwor.HP>0))cout << stwor.HP <<","<<HP2 << endl;
@@ -114,18 +135,8 @@ do
     }
     }while((stwor.HP>=0)&&(HP2>=0)&&(stwor.HP!=0)&&(HP2!=0));
 
-    }
-    else if(zdarzenie==1)//Nic
-    {
-        cout<<"Nic tu nie ma.Idź dalej"<<endl;
-    }
-    else if(zdarzenie==2)//Przedmiot
-    {
-        cout<<"Znalazłeś przedmiot..."<<endl<<"ale nie został jeszcze wprowadzony XD"<<endl;
-    }
-    getchar();
-    }
-   cout<<" Game over"<<endl;
+    // }
+   // cout<<" Game over"<<endl;
 
 
     return 0;
